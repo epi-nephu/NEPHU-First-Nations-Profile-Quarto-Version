bd59569559a352fd42f535b5a40260fc88495154
Alana Little <epi.nephu@austin.org.au>
2025-10-20 09:35:21 +1100

    * Addition of consultation boxes section * Minor edits

--- footnotes.R
+++ footnotes.R
@@ -5,7 +5,16 @@
 # Footnotes used across multiple indicators/sections of report
 
 # ------------------------------------------------------------------------------
-# Data sources
+# Consultation boxes - data notes and caveats
+# ------------------------------------------------------------------------------
+note_box_olderage <- glue::glue("
+Older Aboriginal and/or Torres Strait Islander people are defined as those aged 50 years and over. This is the [age range used by the Australian Institute of Health and Welfare](https://www.aihw.gov.au/reports/older-people/older-australians/contents/population-groups-of-interest/indigenous-australians), because Aboriginal and Torres Strait Islander people are more likely to develop serious medical conditions earlier in life and have a lower life expectancy than non-Indigenous Australians.")
+
+note_box_underidentification <- glue::glue("
+When considering the total number of people who identify as Aboriginal and/or Torres Strait Islander, under-identification is a key consideration. The impact of this is that population numbers are likely to be underestimated, and the true population number is likely to be higher than what is reported here. This needs to be considered not only within this section but throughout this report, and has implications not only for population size, but also for any inferences about service requirements.")
+
+# ------------------------------------------------------------------------------
+# Chart notes - data sources
 # ------------------------------------------------------------------------------
 note_data_census <- glue::glue("
 - Data were obtained from the [ABS Census 2021](https://www.abs.gov.au/census) and are publicly available at Local Government Area (LGA), ABS Statistical Area Level 2 (SA2), and Indigenous Area of Residence (IARE) level.
@@ -15,7 +24,7 @@ note_data_census <- glue::glue("
   - Aboriginal and/or Torres Strait Islander peoples were defined as those who identified themselves as Aboriginal and/or Torres Strait Islander in the Census.")
 
 # ------------------------------------------------------------------------------
-# Caveats and data notes
+# Chart notes - caveats
 # ------------------------------------------------------------------------------
 note_caveat_underidentification <- glue::glue("
 - Reported counts and percentages are a likely underestimate due to under-identification.")
