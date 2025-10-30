07de5ea06f77895e7d4604ba696c35c3681f82d7
Alana Little <epi.nephu@austin.org.au>
2025-10-30 17:21:43 +1100

    First commit

---
+++ functions/tables/f_table_dataprep.R
@@ -0,0 +1,146 @@
+# NEPHU Population Profile - Aboriginal Health
+# Author: Alana Little, NEPHU (alana.little@austin.org.au)
+# Version 2.0, 15/08/2025
+#
+# Data prep for outputting tables
+
+################################################################################
+# Obs/exp tables, by LGA
+################################################################################
+f_preptable_obsexp_lga <- function(data) {
+  
+  data <- data %>% 
+    dplyr::filter(lga_name %in% c(nephu_lga, "NEPHU")) %>% 
+    #
+    dplyr::select(lga_name,
+                  aboriginal_n,
+                  aboriginal_prop,
+                  aboriginal_exp_non,
+                  non_aboriginal_prop,
+                  aboriginal_diff_label)
+  
+  return(data)
+  
+}
+
+################################################################################
+# Obs/exp tables, by age and sex
+################################################################################
+f_preptable_obsexp_agesex <- function(data) {
+  
+  data <- data %>% 
+    dplyr::filter(age_group %in% c(age_group_lvl, "Total")) %>% 
+    #
+    dplyr::select(age_group,
+                  sex,
+                  aboriginal_n,
+                  aboriginal_prop,
+                  aboriginal_exp_non,
+                  non_aboriginal_prop,
+                  aboriginal_diff_label)
+  
+  return(data)
+  
+}
+
+################################################################################
+# Obs/exp tables, by sex
+################################################################################
+f_preptable_obsexp_sex <- function(data) {
+  
+  data <- data %>% 
+    dplyr::select(sex,
+                  aboriginal_n,
+                  aboriginal_prop,
+                  aboriginal_exp_non,
+                  non_aboriginal_prop,
+                  aboriginal_diff_label)
+  
+  return(data)
+  
+}
+
+################################################################################
+# Simple counts and percentages, by LGA
+################################################################################
+f_preptable_simple_lga <- function(data) {
+ 
+  data <- data %>% 
+    dplyr::filter(lga_name %in% c(nephu_lga, "NEPHU")) %>%
+    #
+    dplyr::select(lga_name,
+                  aboriginal_n,
+                  aboriginal_prop)
+  
+  return(data)
+
+}
+
+################################################################################
+# Simple counts and percentages, by age and sex
+################################################################################
+f_preptable_simple_agesex <- function(data) {
+  
+  data <- data %>% 
+    dplyr::filter(age_group %in% c(age_group_lvl, "Total")) %>%
+    #
+    dplyr::select(age_group,
+                  sex,
+                  aboriginal_n,
+                  aboriginal_prop)
+  
+  return(data)
+  
+}
+
+################################################################################
+# Columns grouped by sex
+################################################################################
+f_preptable_grouped_agesex <- function(data) {
+  
+  data <- data %>% 
+    dplyr::select(age_group,
+                  sex,
+                  aboriginal_n,
+                  aboriginal_prop) %>%
+    #
+    tidyr::pivot_wider(names_from  = sex,
+                       values_from = c("aboriginal_n", "aboriginal_prop"),
+                       names_vary  = "slowest") %>% 
+    #
+    janitor::clean_names() %>% 
+    #
+    dplyr::mutate(aboriginal_n_total = aboriginal_n_female + aboriginal_n_male)
+  
+  return(data)
+
+}
+
+################################################################################
+# Columns grouped by Indigenous status, n/% in a single column ("unit")
+################################################################################
+f_preptable_grouped_indigenous <- function(data, x_variable) {
+  
+  x_variable <- rlang::enquo(x_variable)
+  
+  data <- data %>% 
+    dplyr::select(-indigenous_status) %>% 
+    #
+    pivot_wider(names_from  = c("indigenous_label", "unit"),
+                names_vary  = "slowest",
+                values_from = "vic") %>% 
+    #
+    janitor::clean_names() %>%
+    #
+    dplyr::select(!!x_variable,
+                  aboriginal_n,
+                  aboriginal_prop,
+                  non_indigenous_n,
+                  non_indigenous_prop) %>% 
+    #
+    dplyr::arrange(!!x_variable)
+  
+  return(data)
+
+} 
+
---
+++ functions/tables/f_table_grouped.R
@@ -0,0 +1,82 @@
+# NEPHU Population Profile - Aboriginal Health
+# Author: Alana Little, NEPHU (alana.little@austin.org.au)
+# Version 2.0, 15/08/2025
+#
+# Table type: grouped tables
+
+################################################################################
+# By sex
+################################################################################
+f_table_grouped_sex <- function(data, n_variable, col_header, total_row) {
+  
+  if (n_variable == "count") {
+    
+    table_headers <- c("Count", "Percent", "Count", "Percent", "Total")
+
+  }
+  
+  if (n_variable == "estimate") {
+    
+    table_headers <- c("Estimate", "Percent", "Estimate", "Percent", "Total")
+  
+  }
+  
+  table <- data %>% 
+    knitr::kable(format      = "html",
+                 format.args = list(big.mark = ","),
+                 align       = "lrrrrrr",
+                 col.names   = c(col_header, table_headers),
+                 escape      = FALSE) %>%
+    #
+    kableExtra::kable_styling(bootstrap_options = c("hover", "responsive"),
+                              #
+                              full_width = FALSE,
+                              position   = "center",
+                              html_font  = "Arial",
+                              font_size  = 12) %>%
+    #
+    kableExtra::row_spec(row  = c(0, total_row),
+                         bold = TRUE) %>%    
+    #
+    kableExtra::add_header_above(c(" "      = 1,
+                                   "Female" = 2,
+                                   "Male"   = 2,
+                                   "Total"  = 1)) %>% 
+    #
+    kableExtra::column_spec(2:6,
+                            width = "1in")
+  
+  return(table)
+
+}
+
+################################################################################
+# By Indigenous status
+################################################################################
+f_table_grouped_indigenous <- function(data, col_header, total_row) {
+  
+  table <- data %>% 
+    knitr::kable(format      = "html",
+                 format.args = list(big.mark = ","),
+                 align       = "lrrrrrr",
+                 col.names   = c(col_header, "Yes (n)", "Yes (%)", "No (n)", "No (%)"),
+                 escape      = FALSE) %>%
+    #
+    kableExtra::kable_styling(bootstrap_options = c("hover", "responsive"),
+                              #
+                              full_width = FALSE,
+                              position   = "center",
+                              html_font  = "Arial",
+                              font_size  = 12) %>%
+    #
+    kableExtra::row_spec(row  = c(0, total_row),
+                         bold = TRUE) %>%    
+    #
+    kableExtra::add_header_above(c(" " = 1,
+                                   "Identified as Aboriginal and/or Torres Strait Islander" = 4),
+                                 align = "right")
+  
+  return(table)
+  
+}
+
