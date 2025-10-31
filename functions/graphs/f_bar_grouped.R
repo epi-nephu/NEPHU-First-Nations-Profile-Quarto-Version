c5f9ad1f0fa90ba2df0e63f7aa9ca01bfbf9f4ba
Alana Little <epi.nephu@austin.org.au>
2025-10-31 17:32:22 +1100

    Added f_bar_grouped_indigenous()

--- functions/graphs/f_bar_grouped.R
+++ functions/graphs/f_bar_grouped.R
@@ -121,6 +121,80 @@ f_bar_grouped_agesex <- function(data, y_variable, y_max, y_breaks, y_expand, y_
 }
 
 ################################################################################
+# By Indigenous status
+################################################################################
+f_bar_grouped_indigenous <- function(data, x_variable, y_variable, y_max, y_breaks, y_expand, y_title, x_angle = 90, legend_offset = -0.3) {
+  
+  x_variable <- rlang::enquo(x_variable)
+  y_variable <- rlang::enquo(y_variable)
+  
+  if (rlang::as_name(y_variable) == "n") {
+    
+    data <- data %>% 
+      dplyr::mutate(hover_text = paste0(!!x_variable, "\n",
+                                        indigenous_label, "\n",
+                                        "Count: ", format(!!y_variable, big.mark = ",")))
+    
+  }
+  
+  if (rlang::as_name(y_variable) == "estimate") {
+    
+    data <- data %>% 
+      dplyr::mutate(hover_text = paste0(!!x_variable, "\n",
+                                        indigenous_label, "\n",
+                                        "Count: ", format(!!y_variable, big.mark = ",")))
+    
+  }
+  
+  if (rlang::as_name(y_variable) == "prop") {
+    
+    data <- data %>% 
+      dplyr::mutate(hover_text = paste0(!!x_variable, "\n",
+                                        indigenous_label, "\n",
+                                        "Percentage: ", sprintf("%.1f", !!y_variable)))
+    
+  }
+  
+  figure <- data %>% 
+    ggplot(aes(x = !!x_variable, y = !!y_variable, group = indigenous_status, fill = indigenous_status, text = hover_text)) +
+    #
+    geom_col(position = "dodge",
+             col      = colour_gray) +
+    #
+    scale_y_continuous(limits = c(0, y_max),
+                       breaks = scales::breaks_width(y_breaks),
+                       expand = expansion(add = c(0, y_expand)),
+                       labels = scales::comma_format(big.mark = ",")) +
+    #
+    scale_fill_manual(values = c(colour_aboriginal, colour_nonaboriginal),
+                      guide  = "none") +
+    #
+    labs(x = NULL,
+         y = NULL) +
+    #
+    theme_classic() +
+    #
+    theme(axis.text.x  = element_text(angle = x_angle))
+  
+  figure <- figure %>% 
+    plotly::ggplotly(tooltip = "text") %>%
+    #
+    plotly::layout(hovermode = "x",
+                   #
+                   yaxis = list(title = list(text     = y_title,
+                                             font     = list(size = 13, family = "Arial"),
+                                             standoff = 10)),
+                   legend = list(x = 0.5,
+                                 y = legend_offset,
+                                 #
+                                 orientation = "h",
+                                 xanchor     = "center"))
+  
+  return(figure)
+  
+}
+
+################################################################################
 # Combine count and percentage charts into two-panel figure
 ################################################################################
 f_bar_grouped_combine <- function(figure_number, figure_percent, legend_offset = -0.3) {
