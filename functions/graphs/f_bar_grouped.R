5d97b0095aaf06e6e22bf77b7d657dce95571b32
Alana Little <epi.nephu@austin.org.au>
2025-11-05 15:30:14 +1100

    Added f_bar_grouped_sex function

--- functions/graphs/f_bar_grouped.R
+++ functions/graphs/f_bar_grouped.R
@@ -121,6 +121,71 @@ f_bar_grouped_agesex <- function(data, y_variable, y_max, y_breaks, y_expand, y_
 }
 
 ################################################################################
+# By sex
+################################################################################
+f_bar_grouped_sex <- function(data, x_variable, y_variable, y_max, y_breaks, y_expand, y_title, x_angle = 0, legend_offset = -0.1) {
+  
+  x_variable <- rlang::enquo(x_variable)
+  y_variable <- rlang::enquo(y_variable)
+  
+  if (rlang::as_name(y_variable) == "n") {
+    
+    data <- data %>% 
+      dplyr::mutate(hover_text = paste0(sex, "\n",
+                                        answer, "\n",
+                                        "Estimate: ", format(!!y_variable, big.mark = ",")))
+    
+  }
+  
+  if (rlang::as_name(y_variable) == "prop") {
+    
+    data <- data %>% 
+      dplyr::mutate(hover_text = paste0(sex, "\n",
+                                        answer, "\n",
+                                        "Percentage: ", sprintf("%.1f", !!y_variable)))
+    
+  }
+  
+  figure <- data %>% 
+    ggplot(aes(x = !!x_variable, y = !!y_variable, group = sex, fill = sex, text = hover_text)) +
+    #
+    geom_col(position = "dodge",
+             col      = colour_gray) +
+    #
+    scale_y_continuous(limits = c(0, y_max),
+                       breaks = scales::breaks_width(y_breaks),
+                       expand = expansion(add = c(0, y_expand)),
+                       labels = scales::comma_format(big.mark = ",")) +
+    #
+    scale_fill_manual(values = c(colour_female, colour_male),
+                      guide  = "none") +
+    #
+    labs(x = NULL,
+         y = NULL) +
+    #
+    theme_classic() +
+    #
+    theme(axis.text.x = element_text(angle = x_angle))
+  
+  figure <- figure %>% 
+    plotly::ggplotly(tooltip = "text") %>%
+    #
+    plotly::layout(hovermode = "x",
+                   #
+                   yaxis = list(title = list(text     = y_title,
+                                             font     = list(size = 13, family = "Arial"),
+                                             standoff = 10)),
+                   legend = list(x = 0.5,
+                                 y = legend_offset,
+                                 #
+                                 orientation = "h",
+                                 xanchor     = "center"))
+  
+  return(figure)
+  
+}
+
+################################################################################
 # By Indigenous status
 ################################################################################
 f_bar_grouped_indigenous <- function(data, x_variable, y_variable, y_max, y_breaks, y_expand, y_title, x_angle = 90, legend_offset = -0.3) {
