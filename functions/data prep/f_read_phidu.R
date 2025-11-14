985d166e06ffba0e9749a8a6436f5892b0cbea1d
Alana Little <epi.nephu@austin.org.au>
2025-11-14 17:24:12 +1100

    Added f_read_phidu_iare_hospital function

--- functions/data prep/f_read_phidu.R
+++ functions/data prep/f_read_phidu.R
@@ -68,3 +68,74 @@ f_read_phidu_iare <- function(data_sheet, column_select) {
   
 }
 
+# ------------------------------------------------------------------------------
+# Read in PHIDU hospital data - by IARE
+# ------------------------------------------------------------------------------
+f_read_phidu_iare_hospital <- function(data_sheet, column_select) {
+  
+  data <- readxl::read_xls(file.path(root_folder, subfolder_phidu, "phidu_atsi_data_ia_aust.xls"),
+                           sheet        = data_sheet,
+                           trim_ws      = TRUE,
+                           .name_repair = "minimal") %>%
+    #
+    janitor::clean_names() %>%
+    #
+    dplyr::select(iare_name = dplyr::all_of(names(.)[2]),
+                  aboriginal_n = which(names(.) == column_select),
+                  aboriginal_rate = which(names(.) == column_select) + 1) %>% 
+    #
+    dplyr::filter(iare_name %in% c("Craigieburn - Sunbury",
+                                   "Knox",
+                                   "Maroondah",
+                                   "Melbourne - East",
+                                   "Melbourne - North-East",
+                                   "Melbourne - Port Phillip",
+                                   "Moreland - Broadmeadows",
+                                   "Northcote - Preston - Whittlesea",
+                                   "Whitehorse",
+                                   "Yarra Ranges",
+                                   #
+                                   "Greater Melbourne",
+                                   "Rest of Victoria",
+                                   "Victoria")) %>%
+    #
+    dplyr::mutate(
+      iare_name = dplyr::case_when(
+        iare_name == "Craigieburn - Sunbury"            ~ "Craigieburn/Sunbury",
+        iare_name == "Melbourne - East"                 ~ "Melbourne East",
+        iare_name == "Melbourne - North-East"           ~ "Melbourne North-East",
+        iare_name == "Melbourne - Port Phillip"         ~ "Melbourne/Port Phillip",
+        iare_name == "Moreland - Broadmeadows"          ~ "Moreland/Broadmeadows",
+        iare_name == "Northcote - Preston - Whittlesea" ~ "Northcote/Preston/Whittlesea",
+        TRUE ~ iare_name),
+      #
+      iare_name_short = dplyr::case_when(
+        iare_name == "Craigieburn/Sunbury"          ~ "Craigieburn*",
+        iare_name == "Melbourne East"               ~ "Melbourne (E)*",
+        iare_name == "Melbourne North-East"         ~ "Melbourne (NE)*",
+        iare_name == "Melbourne/Port Phillip"       ~ "Port Phillip*",
+        iare_name == "Moreland/Broadmeadows"        ~ "Broadmeadows*",
+        iare_name == "Northcote/Preston/Whittlesea" ~ "Northcote*",
+        TRUE ~ iare_name))
+  
+  data <- data %>%
+    dplyr::mutate(across(where(is.character), ~na_if(., "#")),
+                  across(where(is.character), ~na_if(., "..")),
+                  across(where(is.character), ~na_if(., "n.a."))) %>%
+    #
+    dplyr::mutate(across(!c(iare_name, iare_name_short), as.numeric)) %>%
+    #
+    dplyr::mutate(hospital_ratio = aboriginal_rate / aboriginal_rate[12] * 100) %>% 
+    #
+    dplyr::mutate(across(ends_with("_n"), ~ round(.x, digits = 0))) %>% 
+    #
+    dplyr::select(iare_name,
+                  iare_name_short,
+                  aboriginal_n,
+                  aboriginal_rate,
+                  hospital_ratio)
+  
+  return(data)
+  
+}
+
