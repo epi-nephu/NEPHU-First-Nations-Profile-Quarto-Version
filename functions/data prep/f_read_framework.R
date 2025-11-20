a6879e929f31105446e25f50b78427e4a79a8f5c
Alana Little <epi.nephu@austin.org.au>
2025-11-20 17:01:01 +1100

    First commit

---
+++ functions/data prep/f_read_framework.R
@@ -0,0 +1,63 @@
+# NEPHU Population Profile - Aboriginal Health
+# Author: Alana Little, NEPHU (alana.little@austin.org.au)
+# Version 2.0, 15/08/2025
+#
+# Code for importing and wrangling data
+# Data source: AIHW Aboriginal and Torres Strait Islander Health Performance Framework
+# https://www.indigenoushpf.gov.au/
+#
+# ------------------------------------------------------------------------------
+# Read in HPF data - vaccination
+# ------------------------------------------------------------------------------
+f_read_hpf_vaccination <- function(data_sheet, column_select) {
+  
+  data <- readxl::read_excel(file.path(root_folder, subfolder_framework, "302-Immunisation.xlsx"),
+                             sheet        = data_sheet,
+                             skip         = 2,
+                             trim_ws      = TRUE,
+                             .name_repair = "universal_quiet") %>% 
+    #
+    janitor::clean_names() %>%
+    #
+    dplyr::filter(row_number() %in% c(8:9)) %>% 
+    #
+    dplyr::select(!!!column_select)
+
+  return(data)
+  
+}
+
+# ------------------------------------------------------------------------------
+# Wrangle HPF data - vaccination
+# ------------------------------------------------------------------------------
+f_wrangle_hpf_vaccination <- function(data) {
+  
+  data <- data %>% 
+    dplyr::mutate(indigenous_status = dplyr::case_when(
+    indigenous_status == "Indigenous" ~ "aboriginal",
+    TRUE ~ "non_aboriginal")) %>%
+    #
+    dplyr::mutate(across(!indigenous_status, as.numeric)) %>% 
+    #
+    tidyr::pivot_longer(!indigenous_status,
+                        names_to  = "vaccine",
+                        values_to = "prop") %>% 
+    #
+    dplyr::mutate(
+      indigenous_label = dplyr::case_when(
+        indigenous_status == "aboriginal"     ~ "Aboriginal",
+        indigenous_status == "non_aboriginal" ~ "Non-Indigenous",
+        TRUE ~ NA_character_),
+      #
+      indigenous_status = factor(dplyr::case_when(
+        indigenous_status == "aboriginal"     ~ "Identified as Aboriginal\nand/or Torres Strait Islander",
+        indigenous_status == "non_aboriginal" ~ "Did not identify as Aboriginal\nand/or Torres Strait Islander",
+        TRUE ~ NA_character_),
+        #
+        levels = c("Identified as Aboriginal\nand/or Torres Strait Islander",
+                   "Did not identify as Aboriginal\nand/or Torres Strait Islander")))
+  
+  return(data)
+  
+}
+
