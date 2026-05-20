5f77a8a2f606848577b27c035b5d9de16f79abf9
Alana Little <epi.nephu@austin.org.au>
2026-05-20 17:16:00 +1000

    Renamed file

---
+++ notes/notes_definitions_denominator.R
@@ -0,0 +1,17 @@
+# NEPHU Population Profile - Aboriginal Health
+# Author: Alana Little, NEPHU (alana.little@austin.org.au)
+
+# Commonly used denominator definitions
+
+note_denominator_census_lga <- glue::glue("
+Total number of people")
+
+note_denominator_census_lga_aboriginal <- glue::glue("
+Total number of Aboriginal and/or Torres Strait Islander people")
+
+note_denominator_census_agesex <- glue::glue("
+Total number of people in the age/sex group")
+
+note_denominator_census_agesex_aboriginal <- glue::glue("
+Total number of Aboriginal and/or Torres Strait Islander people in the age/sex group")
+
