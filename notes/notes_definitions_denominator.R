19dadb79bb9e908895fc13e35c6958dc195409b9
Alana Little <epi.nephu@austin.org.au>
2026-06-03 17:00:19 +1000

    Minor edits

--- notes/notes_definitions_denominator.R
+++ notes/notes_definitions_denominator.R
@@ -25,9 +25,9 @@ note_disaggregation_lga  <- "Local Government Area (LGA)"
 note_disaggregation_sa2  <- "Statistical Area Level 2 (SA2)"
 note_disaggregation_iare <- "Indigenous Area of Residence (IARE)"
 
+note_disaggregation_agesex <- "Age and sex"
 note_disaggregation_age    <- "Age"
 note_disaggregation_sex    <- "Sex"
-note_disaggregation_agesex <- "Age and sex"
 
 note_disaggregation_grade <- "School year (grade)"
 note_disaggregation_year  <- "Year"
@@ -36,4 +36,7 @@ note_disaggregation_year  <- "Year"
 # Commonly used comparison definitions
 ################################################################################
 note_comparison_aboriginal <- "Aboriginal and/or Torres Strait Islander status"
+
+note_comparison_age <- "Age"
+note_comparison_sex <- "Sex"
 
