2a569aa7ec7d63e9670294c4ab78cb2fae7c5f70
Alana Little <epi.nephu@austin.org.au>
2026-06-05 16:21:21 +1000

    Minor edits

--- notes/notes_definitions_other.R
+++ notes/notes_definitions_other.R
@@ -24,6 +24,8 @@ note_numerator_category_population <- "Number of people in each category"
 note_denominator_census_aboriginal <- "Total number of Aboriginal and/or Torres Strait Islander people who completed the Census"
 note_denominator_census_population <- "Total number of people who completed the Census"
 
+note_denominator_erp <- "Estimated total number of people in the population for the specified year"
+
 note_denominator_natsihs <- "Total number of Aboriginal and/or Torres Strait Islander people who answered this question/these questions in the National Aboriginal and Torres Strait Islander Health Survey"
 
 note_denominator_perinatal <- "Total number of Aboriginal and/or Torres Strait Islander women who had at least one pregnancy between January 2019 and December 2021"
